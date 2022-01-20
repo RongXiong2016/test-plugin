35768944a42e23fcb7026e37e2790a9eb12f35c6
GitHub Action <action@github.com>
2022-01-20 11:43:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/rongxiong2016/testplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rongxiong2016/testplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rongxiong2016.testplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rongxiong2016.testplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/rongxiong2016/testplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rongxiong2016/testplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rongxiong2016.testplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rongxiong2016.testplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
